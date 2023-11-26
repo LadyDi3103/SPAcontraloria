a2813e72b7beab5864af41363ad08f76ba21a2de
LadyDi3103 <chinamy2004@gmail.com>
2023-11-26 18:52:50 -0500

    ADD: footer

---
+++ src/templates/footer.js
@@ -0,0 +1,10 @@
+// import { cat } from '../images.js';
+// import cat from '../img/github.png';
+const Footer = () => {
+  const viewFooter = `
+
+    ©Derechos Reservados 2023
+    `;
+  return viewFooter;
+};
+export default Footer;
