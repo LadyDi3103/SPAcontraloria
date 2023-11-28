b1a6ed681b5dc439281ebc053af70e8e31ed23c8
LadyDi3103 <chinamy2004@gmail.com>
2023-11-28 08:06:15 -0500

    ADD: Monitor login

--- src/login.js
+++ src/login.js
@@ -1,31 +1,30 @@
-
-const monitorLoginLink = document.getElementById('login');
-const showModal = document.getElementById('container__login');
-const hideModal = document.getElementById('button-login');
+const buttonLogin = document.getElementById('button-login');
 const homeAdminSection = document.getElementById('container__monitor');
 
-monitorLoginLink.addEventListener('click', () => {
-    showModal.style.display = 'block';
-});
-
-hideModal.addEventListener('click', () => {
-    showModal.style.display = 'none';
-});
-
 // Función para manejar el inicio de sesión
-function login() {
-    const email = document.getElementById('mail').value;
-    const password = document.getElementById('password').value;
+buttonLogin.addEventListener('click', handleLogin);
+function handleLogin() {
 
-    if (email === 'beatriz@monitor.pe' && password === '123456') {
-        // Autenticación exitosa
-        alert('Inicio de sesión exitoso');
-        showModal.style.display = 'none'; // Oculta el modal
+    const emailInput = document.getElementById('mail');
+    const passwordInput = document.getElementById('password');
 
-        // Muestra la sección home-admin
-        homeAdminSection.style.display = 'block';
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    // Validar que ambos campos estén llenos
+    if (email.trim() === '' || password.trim() === '') {
+        alert('Por favor, ingresa tu email y contraseña.');
+        return;
+    }
+
+    // Validar credenciales
+ else if (email === 'beatriz@monitor.pe' && password === '123456') {
+        // Redirigir a la página del monitor
+        window.location.href = 'indexMonitor.html';
     } else {
         // Credenciales incorrectas
         alert('Credenciales incorrectas. Intenta de nuevo.');
+        // Limpiar campos de contraseña después de un intento fallido
+        passwordInput.value = '';
     }
 }
