d19d7138caa41e0ac3f7fe6a9308afbfccacacb4
LadyDi3103 <chinamy2004@gmail.com>
2023-11-28 07:39:00 -0500

    ADD: login.js

--- src/main.js
+++ src/main.js
@@ -197,34 +197,3 @@ const crearMapaYUbicar = (obj) => {
 };
 
 conseguirObras();
-
-const monitorLoginLink = document.getElementById('login');
-const showModal = document.getElementById('container__login');
-const hideModal = document.getElementById('button-login');
-const homeAdminSection = document.getElementById('container__monitor');
-
-monitorLoginLink.addEventListener('click', () => {
-    showModal.style.display = 'block';
-});
-
-hideModal.addEventListener('click', () => {
-    showModal.style.display = 'none';
-});
-
-// Función para manejar el inicio de sesión
-function login() {
-    const email = document.getElementById('mail').value;
-    const password = document.getElementById('password').value;
-
-    if (email === 'beatriz@monitor.pe' && password === '123456') {
-        // Autenticación exitosa
-        alert('Inicio de sesión exitoso');
-        showModal.style.display = 'none'; // Oculta el modal
-
-        // Muestra la sección home-admin
-        homeAdminSection.style.display = 'block';
-    } else {
-        // Credenciales incorrectas
-        alert('Credenciales incorrectas. Intenta de nuevo.');
-    }
-}
