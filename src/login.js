c5bed19a12ec1ed1c8a8d1b82f3ca29ffbe312f0
LadyDi3103 <chinamy2004@gmail.com>
2023-11-29 10:29:27 -0500

    ADD: styles to login section

--- src/login.js
+++ src/login.js
@@ -1,91 +1,10 @@
-// document.addEventListener('DOMContentLoaded', conseguirUser() {
-//     // Aquí debes tener la definición de handleLogin
-//     function handleLogin() {
-//             const emailInput = document.getElementById('mail');
-//             const passwordInput = document.getElementById('password');
-        
-//             const email = emailInput.value;
-//             const password = passwordInput.value;
-        
-//             // Validar que ambos campos estén llenos
-//             if (email.trim() === '' || password.trim() === '') {
-//                 alert('Por favor, ingresa tu email y contraseña.');
-//                 return;
-//             }
-        
-//             // Validar credenciales
-//          else if (email === user.user1 && password === user.psw) {
-//                 // Redirigir a la página del monitor
-//                 window.location.href = 'indexMonitor.html';
-//             } else {
-//                 // Credenciales incorrectas
-//                 alert('Credenciales incorrectas. Intenta de nuevo.');
-//                 // Limpiar campos de contraseña después de un intento fallido
-//                 passwordInput.value = '';
-//             }
-//         }
-//         // Lógica de inicio de sesión
-//     }
-// );
-
-
-
-
-// const conseguirUser = () => {
-//     return fetch('https://books-tsfn.onrender.com/user/1')
-//       .then(response => response.json())
-//       .then(puente => {
-//             console.log(puente)
-//             user = {
-//                 user1: puente.dislike_total,
-//                 psw: puente.id
-//             };
-
-//         }
-      
-//         );
-   
-// };
-
-
-
-// const buttonLogin = document.getElementById('button-login');
-// const homeAdminSection = document.getElementById('container__monitor');
-
-// // Función para manejar el inicio de sesión
-// buttonLogin.addEventListener('click', conseguirUser());
-
-// function handleLogin() {
-//     const emailInput = document.getElementById('mail');
-//     const passwordInput = document.getElementById('password');
-
-//     const email = emailInput.value;
-//     const password = passwordInput.value;
-
-//     // Validar que ambos campos estén llenos
-//     if (email.trim() === '' || password.trim() === '') {
-//         alert('Por favor, ingresa tu email y contraseña.');
-//         return;
-//     }
-
-//     // Validar credenciales
-//  else if (email === user.user1 && password === user.psw) {
-//         // Redirigir a la página del monitor
-//         window.location.href = 'indexMonitor.html';
-//     } else {
-//         // Credenciales incorrectas
-//         alert('Credenciales incorrectas. Intenta de nuevo.');
-//         // Limpiar campos de contraseña después de un intento fallido
-//         passwordInput.value = '';
-//     }
-// }
-
 let user 
 
 document.addEventListener('DOMContentLoaded', function() {
     const emailInput = document.getElementById('mail');
     const passwordInput = document.getElementById('password');
-
+    const errorEmail = document.getElementById('errorEmail');
+    const errorPassword = document.getElementById('errorPassword');
     // Obtener usuario al cargar la página
     obtenerUsuario();
 
@@ -130,7 +49,6 @@ console.log(password, "Password");
                 };
                 console.log(user);
             })
-         
             .catch(error => console.error('Error al obtener el usuario:', error));
     }
 });
